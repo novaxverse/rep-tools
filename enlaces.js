95b7011a20b210405a47fcc9acbc732c16b553cc
novaxverse <novaxverse13@gmail.com>
2026-09-18 19:14:11 +0100

    Add files via upload

--- enlaces.js
+++ enlaces.js
@@ -25,7 +25,7 @@ const tools = [
     type: "PS4 Tool",
 
     // ⬇️ Enlace de MediaFire — descarga 
-    url: "https://github.com/novaxverse/rep-tools/releases/download/vR2/Store-R2.pkg"
+    url: "https://pkg-zone.com/install"
   },
 
   {
